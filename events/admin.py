92594b4d4bdb178d4664436f941c8efa9b8de4c3
Sabith <sabithkarayil123@gmail.com>
2025-01-03 15:11:28 +0530

    create group section

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Department, Student, Category, Result, Schedule, Image
+from .models import Department, Posters, Student, Category, Result, Schedule, Image
 
 admin.site.register(Department)
 admin.site.register(Student)
@@ -8,3 +8,4 @@ admin.site.register(Category)
 admin.site.register(Result)
 admin.site.register(Schedule)
 admin.site.register(Image)
+admin.site.register(Posters)
